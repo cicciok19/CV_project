dca8f90764cafb3606a343a666f1ab6ced550c2c
Pietro <pingi.pie95@gmail.com>
2020-10-13 12:07:16 +0200

    Haar does not go. FUCK

--- VideoMani.py
+++ VideoMani.py
@@ -19,13 +19,19 @@ def bgr_to_rgb(image):
     """
     return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-def haarCascade():
+def haarCascade(image, gray):
     hand_cascade = cv2.CascadeClassifier("hand.xml")
     palm_cascade = cv2.CascadeClassifier("palm.xml")
     hand = hand_cascade.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in hand:
-        cv2.rectangle()
-    return
+        cv2.rectangle(image, (x,y), (x+w, y+h), (255,0,0), 2) #x,y are the starting point; w,h width and height
+        #we create the region of image of the hand because the palm is always inside the hand
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = image[y:y+h, x:x+w]
+        palm = palm_cascade.detectMultiScale(roi_gray)
+        for (px, py, pw, ph) in palm:
+            cv2.rectangle(roi_color, (px, py), (px+pw, py+ph), (0,255,0), 2)
+    return image, gray
 
 def main():
     #setting the camera
@@ -54,14 +60,18 @@ def main():
         frame = grab_frame(cap)
         if img is None:
             # convert it in RBG (for Matplotlib)
-            img = plt.imshow(bgr_to_rgb(frame))
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            haar_image, haar_gray = haarCascade(frame, gray)
+            img = plt.imshow(bgr_to_rgb(haar_image))
             plt.axis("off")  # hide axis, ticks, ...
             plt.title("Camera Capture")
             # show the plot!
             plt.show()
         else:
             # set the current frame as the data to show
-            img.set_data(bgr_to_rgb(frame))
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            haar_image, haar_gray = haarCascade(frame, gray)
+            img.set_data(bgr_to_rgb(haar_image))
             # update the figure associated to the shown plot
             fig.canvas.draw()
             plt.pause(1/30)  # pause: 30 frames per second
